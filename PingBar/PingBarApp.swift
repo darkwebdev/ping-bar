53bd6dce3fff2f3729158e4966fcae37ac5ac0ff
tmanyanov <2737310+darkwebdev@users.noreply.github.com>
2025-09-11 18:35:27 +0200

    Initial Commit

---
+++ PingBar/PingBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingBarApp.swift
+//  PingBar
+//
+//  Created by Manyanov, Timur on 11.09.25.
+//
+
+import SwiftUI
+
+@main
+struct PingBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PingBarTests/PingBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  PingBarTests.swift
+//  PingBarTests
+//
+//  Created by Manyanov, Timur on 11.09.25.
+//
+
+import Testing
+@testable import PingBar
+
+struct PingBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
